f499c1bbe5d3a5844839b38e06505c8641b33f2c
wisniewskikr <wisniewskikr@gmail.com>
2023-10-25 12:19:18 +0200

    Add index and public pages

--- technologies/security/gui/html/js-express-gui-html-helloworld-security-basic-auth/app.js
+++ technologies/security/gui/html/js-express-gui-html-helloworld-security-basic-auth/app.js
@@ -6,7 +6,11 @@ const app = express();
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")))
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend/html", "helloworld.html"));
+  res.sendFile(path.resolve(__dirname, "frontend/html", "index.html"));
+})
+
+app.get("/public", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend/html", "public.html"));
 })
 
 app.all("*", (req, res) => {
