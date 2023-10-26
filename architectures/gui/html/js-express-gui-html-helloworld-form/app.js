fdb9cbfc242f743df12f1fde3613f181ec276ffe
wisniewskikr <wisniewskikr@gmail.com>
2023-10-26 19:45:28 +0200

    Init project

--- architectures/gui/html/js-express-gui-html-helloworld-form/app.js
+++ architectures/gui/html/js-express-gui-html-helloworld-form/app.js
@@ -1,12 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const port = 3000;
 
 const app = express();
 app.use(express.static('static'));
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req, res) => {
+  res.locals.name = null
   res.render('helloworld')
+})
+
+app.post("/", (req, res) => {
+  res.locals.name = req.body.name
+  res.render('helloworld')
+})
+
+app.all("*", (req, res) => {
+  res.render('404')
 })
 
 app.listen(port, function(error) {
