b02b21abadc615fd552d788c6e93eacd2643544b
wisniewskikr <wisniewskikr@gmail.com>
2023-10-27 19:23:51 +0200

    Add Login page

--- technologies/security/gui/html/js-express-gui-html-helloworld-security-form/app.js
+++ technologies/security/gui/html/js-express-gui-html-helloworld-security-form/app.js
@@ -12,6 +12,10 @@ app.get("/", (req, res) => {
   res.render('index')
 })
 
+app.get("/login", (req, res) => {
+  res.render('login')
+})
+
 app.get("/public", (req, res) => {
   res.render('public')
 })
