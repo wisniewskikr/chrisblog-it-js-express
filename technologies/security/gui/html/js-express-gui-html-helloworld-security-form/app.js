20b4f6d2948be9c54afac52e7128f1ffef36b1a9
wisniewskikr <wisniewskikr@gmail.com>
2023-10-27 18:56:11 +0200

    Init project

--- technologies/security/gui/html/js-express-gui-html-helloworld-security-form/app.js
+++ technologies/security/gui/html/js-express-gui-html-helloworld-security-form/app.js
@@ -1,24 +1,33 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const path = require('path');
+const { authPage } = require('./middlewares/auth-middleware');
 const port = 3000;
 
 const app = express();
-app.use(express.static('static'));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")))
 
 app.get("/", (req, res) => {
-  res.locals.name = null
-  res.render('helloworld')
+  res.sendFile(path.resolve(__dirname, "frontend/html", "index.html"));
 })
 
-app.post("/", (req, res) => {
-  res.locals.name = req.body.name
-  res.render('helloworld')
+app.get("/public", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend/html", "public.html"));
+})
+
+app.get("/user", authPage(["USER", "ADMIN"]), (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend/html", "user.html"));
+})
+
+app.get("/admin", authPage(["ADMIN"]), (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend/html", "admin.html"));
+})
+
+app.get("/401", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend/html", "401.html"));
 })
 
 app.all("*", (req, res) => {
-  res.render('404')
+  res.sendFile(path.resolve("frontend/html", "404.html"));
 })
 
 app.listen(port, function(error) {
--- technologies/security/gui/html/js-express-gui-html-helloworld-security-form/static/js/helloworld.js
+++
@@ -1,7 +0,0 @@
-const createLink = document.querySelector('#create-link');
-const form = document.querySelector('form');
-
-createLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    form.submit();
-});
