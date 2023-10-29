7da7ca17335ca758e4200fa543e78a1a8ae40f5e
wisniewskikr <wisniewskikr@gmail.com>
2023-10-29 17:53:39 +0100

    Init project

---
+++ technologies/security/gui/html/js-express-gui-html-helloworld-security-form-jwt/app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const { login, logout, authPage } = require('./middlewares/auth-middleware');
+const port = 3000;
+
+const app = express();
+app.use(express.static('static'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+app.get("/", (req, res) => {
+  res.locals.username = req.session.username;
+  res.render('index')
+})
+
+app.get("/login", (req, res) => {
+  displayLogin(req, res);
+})
+
+app.get("/logout", (req, res) => {
+  handleLogout(req, res);
+})
+
+app.post("/login", (req, res) => {
+  handleLogin(req, res);
+})
+
+app.get("/public", (req, res) => {
+  res.render('public')
+})
+
+app.get("/user", authPage(["USER", "ADMIN"]), (req, res) => {
+  res.render('user')
+})
+
+app.get("/admin", authPage(["ADMIN"]), (req, res) => {
+  res.render('admin')
+})
+
+app.get("/401", (req, res) => {
+  res.render('401')
+})
+
+app.all("*", (req, res) => {
+  res.render('404')
+})
+
+app.listen(port, function(error) {
+  if (error) {
+    console.log('Something went wrong', error)
+  } else {
+    console.log('Server is listening on port ' + port)
+  }
+});
+
+// ***** HELP METHODS ***** //
+
+function displayLogin(req, res) {
+  res.locals.error = req.session.error
+  res.render('login');
+}
+
+function handleLogin(req, res) {
+  login(req, res);  
+}
+
+function handleLogout(req, res) {
+  logout(req, res);  
+}
---
+++ technologies/security/gui/html/js-express-gui-html-helloworld-security-form-jwt/middlewares/auth-middleware.js
@@ -0,0 +1,55 @@
+const db = '[{"username": "user", "password": "user123", "roles": ["USER"]}, {"username": "admin", "password": "admin123", "roles": ["USER", "ADMIN"]}]'
+
+const login = (req, res) => {
+
+    let username = req.body.username;
+	let password = req.body.password;
+
+    const roles = getRolesByUsernameAndPassword(username, password)
+
+    if ( roles != null ) {
+        req.session.roles = roles;
+		req.session.username = username;       
+        res.redirect("/");
+    } else {
+        req.session.error = 'Wrong username or password';
+        res.redirect('/login');
+    }
+
+}
+
+const logout = (req, res) => {
+
+    req.session.roles = null;
+	req.session.username = null;
+    res.redirect("/");
+
+}
+
+const authPage = (permissions) => {
+
+    return (req, res, next) => {
+
+        const roles = req.session.roles;
+        const found = ( roles == null) ? false : permissions.some(r=> roles.includes(r));
+        if ( !found ) {
+            res.redirect('/401')
+        } else {            
+            next()
+        }         
+
+    }
+
+}
+
+function getRolesByUsernameAndPassword(username, password) {
+
+    var user = JSON.parse(db).find(function(e) {
+        return e.username == username && e.password == password
+    })
+
+    return (user != null) ? user.roles : null;
+
+}
+
+module.exports = { login, logout, authPage };
