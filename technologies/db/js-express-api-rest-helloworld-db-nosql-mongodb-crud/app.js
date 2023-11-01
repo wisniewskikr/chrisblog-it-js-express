d4059498af034a0ae44aa1b869ae738bc21c85b7
wisniewskikr <wisniewskikr@gmail.com>
2023-11-01 21:19:54 +0100

    Update app.js

--- technologies/db/js-express-api-rest-helloworld-db-nosql-mongodb-crud/app.js
+++ technologies/db/js-express-api-rest-helloworld-db-nosql-mongodb-crud/app.js
@@ -1,90 +1,108 @@
-import express from 'express';
-import { getAll, getById, create, update, deleteById } from './services/messages-service.js';
-const port = 3000;
+const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
+dotenv.config()
 
-app.get("/api/v1/messages", async (req, res) => {
-  const messages = await getAll();
-  displayMessage(messages, res, 200);
-})
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
-app.get("/api/v1/messages/:id", async (req, res) => {
+app.listen(3000, () => console.log('Server Started'))
 
-  const messageId = req.params.id
-  if (isNaN(messageId)) {
-    const json = JSON.parse('{"message": "Specific ID Not Found"}')
-    displayMessage(json, res, 200);
-    return;
-  }
+// const subscribersRouter = require('./routes/subscribers')
+// app.use('/subscribers', subscribersRouter)
 
-  const message = await getById(messageId);
-  if (message == null) {
-    const json = JSON.parse('{"message": "Specific Message Not Found"}')
-    displayMessage(json, res, 200);
-    return;
-  }
+// import express from 'express';
+// import { getAll, getById, create, update, deleteById } from './services/messages-service.js';
+// const port = 3000;
 
-  displayMessage(message, res, 200);
+// const app = express();
+// app.use(express.json());
 
-})
+// app.get("/api/v1/messages", async (req, res) => {
+//   const messages = await getAll();
+//   displayMessage(messages, res, 200);
+// })
 
-app.post("/api/v1/messages", async (req, res) => {
+// app.get("/api/v1/messages/:id", async (req, res) => {
+
+//   const messageId = req.params.id
+//   if (isNaN(messageId)) {
+//     const json = JSON.parse('{"message": "Specific ID Not Found"}')
+//     displayMessage(json, res, 200);
+//     return;
+//   }
+
+//   const message = await getById(messageId);
+//   if (message == null) {
+//     const json = JSON.parse('{"message": "Specific Message Not Found"}')
+//     displayMessage(json, res, 200);
+//     return;
+//   }
+
+//   displayMessage(message, res, 200);
+
+// })
+
+// app.post("/api/v1/messages", async (req, res) => {
   
-  await create(req.body.text);
-  const json = JSON.parse('{"message": "New Message was added"}')
-  displayMessage(json, res, 200);
+//   await create(req.body.text);
+//   const json = JSON.parse('{"message": "New Message was added"}')
+//   displayMessage(json, res, 200);
 
-})
+// })
 
-app.put("/api/v1/messages", async (req, res) => {
+// app.put("/api/v1/messages", async (req, res) => {
   
-  await update(req.body);
-  const json = JSON.parse('{"message": "Message was updated"}')
-  displayMessage(json, res, 200);
+//   await update(req.body);
+//   const json = JSON.parse('{"message": "Message was updated"}')
+//   displayMessage(json, res, 200);
 
-})
+// })
 
-app.delete("/api/v1/messages/:id", async (req, res) => {
+// app.delete("/api/v1/messages/:id", async (req, res) => {
   
-  const messageId = req.params.id
-  if (isNaN(messageId)) {
-    const json = JSON.parse('{"message": "Specific ID Not Found"}')
-    displayMessage(json, res, 200);
-    return;
-  }
+//   const messageId = req.params.id
+//   if (isNaN(messageId)) {
+//     const json = JSON.parse('{"message": "Specific ID Not Found"}')
+//     displayMessage(json, res, 200);
+//     return;
+//   }
 
-  const messageCurrent = getById(messageId);
-  if (!messageCurrent) {
-    const json = JSON.parse('{"message": "Specific Message Not Found"}')
-    displayMessage(json, res, 200);
-    return; 
-  }
+//   const messageCurrent = getById(messageId);
+//   if (!messageCurrent) {
+//     const json = JSON.parse('{"message": "Specific Message Not Found"}')
+//     displayMessage(json, res, 200);
+//     return; 
+//   }
 
-  await deleteById(messageId);
-  const json = JSON.parse('{"message": "Message was deleted"}')
-  displayMessage(json, res, 200); 
+//   await deleteById(messageId);
+//   const json = JSON.parse('{"message": "Message was deleted"}')
+//   displayMessage(json, res, 200); 
 
-})
+// })
 
-app.all("*", (req, res) => {
-  const json = JSON.parse('{"message": "Error: Resource Not Found. Please use following API path: /api/v1/messages"}')
-  displayMessage(json, res, 404);
-})
+// app.all("*", (req, res) => {
+//   const json = JSON.parse('{"message": "Error: Resource Not Found. Please use following API path: /api/v1/messages"}')
+//   displayMessage(json, res, 404);
+// })
 
-app.listen(port, function(error) {
-  if (error) {
-    console.log('Something went wrong', error)
-  } else {
-    console.log('Server is listening on port ' + port)
-  }
-});
+// app.listen(port, function(error) {
+//   if (error) {
+//     console.log('Something went wrong', error)
+//   } else {
+//     console.log('Server is listening on port ' + port)
+//   }
+// });
 
-function displayMessage(json, res, statusCode) {
+// function displayMessage(json, res, statusCode) {
 
-  res.statusCode = statusCode;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(json));
+//   res.statusCode = statusCode;
+//   res.setHeader('Content-Type', 'application/json');
+//   res.end(JSON.stringify(json));
 
-}
+// }
