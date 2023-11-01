615798dae3c8bc155216f8dc863690ee19faa593
wisniewskikr <wisniewskikr@gmail.com>
2023-11-01 07:47:59 +0100

    Update service

--- architectures/api/rest/js-express-api-rest-helloworld-crud/services/messages-service.js
+++ architectures/api/rest/js-express-api-rest-helloworld-crud/services/messages-service.js
@@ -6,7 +6,7 @@ module.exports = {
       return messages;
     },
     getById: function (id) {
-        return messages.find((c) => c.id === id);
+        return messages.find((c) => c.id == id);
     }, 
     create: function (text) {
       const message = JSON.parse(`{"id": ${messageId}, "text": "${text}"}`)
@@ -14,10 +14,10 @@ module.exports = {
       messageId++;
     },
     update: function (message) {
-      messages = messages.filter((c) => c.id !== message.id);
+      messages = messages.filter((c) => c.id != message.id);
       messages.push(message);
     },
     delete: function (id) {
-      messages = messages.filter((c) => c.id !== id);
+      messages = messages.filter((c) => c.id != id);
     }
 };
