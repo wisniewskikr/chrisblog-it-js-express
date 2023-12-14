50e827fdba5357523a006e294e6f4cdc176f6020
wisniewskikr <wisniewskikr@gmail.com>
2023-12-14 07:31:09 +0100

    Remove unnecessary elements

--- technologies/local-storage/js-express-gui-html-helloworld-local-storage/static/js/helloworld.js
+++ technologies/local-storage/js-express-gui-html-helloworld-local-storage/static/js/helloworld.js
@@ -1,21 +1,3 @@
-const nameEl = document.querySelector('#name');
-const messageTextEl = document.querySelector('#message-text');
-const createLink = document.querySelector('#create-link');
-const backLink = document.querySelector('#back-link');
-
-createLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    const name = nameEl.value
-    const message = `Hello World ${name}!`
-    messageTextEl.innerHTML = message
-    createLink.style.display = 'none'
-    backLink.style.display = 'block'
-});
-
-backLink.addEventListener('click', (e) => {
-    e.preventDefault();
-    nameEl.value = ""
-    messageTextEl.innerHTML = null
-    createLink.style.display = 'block'
-    backLink.style.display = 'none'
-});
+const messageEl = document.querySelector('#message');
+const message = `Hello World!`
+messageEl.innerHTML = message
